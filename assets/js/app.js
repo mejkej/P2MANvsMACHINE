a5b74de37e7427cde4dc953a707be8bb279012f0
mejkej <krus3@icloud.com>
2023-02-01 09:57:16 +0000

    cashed the DOM

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const choices = ["rock", "paper", "scissors"];
+const playerHand = document.querySelector('#man-hand');
+const computerHand = document.querySelector('#machine-hand');
+const resultText = document.querySelector('#text-result');
+const playerScore = document.querySelector('#p-score');
+const compScore = document.querySelector('#c-score');
