3f518b08f99dd59e7eeb92cec80a43e5d0d60aa8
mejkej <krus3@icloud.com>
2023-02-06 04:23:26 +0000

    when clicked img apperas in playerhands display

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,13 @@ const computerHand = document.getElementById('machine-hand');
 const resultText = document.getElementById('text-result');
 const playerScore = document.getElementById('p-score');
 const compScore = document.getElementById('c-score');
-const rock = document.getElementById('rock')
-const paper = document.getElementById('paper')
-const scissors = document.getElementById('scissors')
+
+const possiblePicks = document.getElementsByClassName('possiblePick');
+
+let playerPick;
+let computerPick;
+
+Array.from(possiblePicks).forEach(possiblePick => possiblePick.addEventListener('click', (e) => {
+    playerPick = 'assets/images/' + e.target.id + '.png';
+    playerHand.src = playerPick;
+}));
