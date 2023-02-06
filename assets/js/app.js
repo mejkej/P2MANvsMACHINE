ebb1a123086d20c0688bd5663e495ab99adf9fef
mejkej <krus3@icloud.com>
2023-02-06 07:45:24 +0000

    compputer pick gets displayed in computer hand window working on getting the winner

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,41 @@ const compScore = document.getElementById('c-score');
 const possiblePicks = document.getElementsByClassName('possiblePick');
 
 let playerPick;
-let computerPick;
+let compPick;
+
 
 Array.from(possiblePicks).forEach(possiblePick => possiblePick.addEventListener('click', (e) => {
     playerPick = 'assets/images/' + e.target.id + '.png';
     playerHand.src = playerPick;
+    getCompPick()
+    declareWinner()
 }));
+
+function getCompPick() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+    
+    if (randomNumber === 1) {
+      compPick = 'assets/images/rock.png'
+    }
+    if (randomNumber === 2) {
+      compPick = 'assets/images/paper.png'
+    }
+    if (randomNumber === 3) {
+      compPick = 'assets/images/scissors.png'
+    }
+    computerHand.src = compPick;
+  }
+
+  function declareWinner(){
+    if (compPick === playerPick){
+        resultText.innerHTML = 'DRAW!'
+    }
+      if (compPick === 'assets/images/rock.png' && playerPick === 'assets/images/paper.png'){
+        resultText.innerHTML = 'One point for mankind!'
+      }
+    
+      
+
+
+  }
+
