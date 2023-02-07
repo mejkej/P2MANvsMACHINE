82b80226f63616976db93eff90a8c1ef365eefb8
mejkej <krus3@icloud.com>
2023-02-07 12:32:44 +0000

    second page html and styling a few small tweaks remaining

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const zero = document.getElementById('0')
+const one = document.getElementById('1')
+const two = document.getElementById('2')
+const three = document.getElementById('3')
+const four = document.getElementById('4')
+const five = document.getElementById('5')
+const six = document.getElementById('6')
+const seven = document.getElementById('7')
+const eight = document.getElementById('8')
+
+const squares = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+let playerPick;
+let compPick;
+
+squares.addEventListener('click', function()){
+    console.log(This)
+}
