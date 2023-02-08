cdf838acb8d0c21f00f9960e6cbea9db636f3e01
mejkej <krus3@icloud.com>
2023-02-08 03:23:04 +0000

    html and styling of page2 more or less done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-const zero = document.getElementById('0')
-const one = document.getElementById('1')
-const two = document.getElementById('2')
-const three = document.getElementById('3')
-const four = document.getElementById('4')
-const five = document.getElementById('5')
-const six = document.getElementById('6')
-const seven = document.getElementById('7')
-const eight = document.getElementById('8')
+const board = document.getElementById('board')
+let unPickedSquareArray = document.getElementById('0, 1, 2, 3, 4, 5, 6, 7, 8,')
+const message = document.getElementById('message')
 
-const squares = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+
+
 let playerPick;
 let compPick;
+let unPickedSquare;
+let pickedSquare = "style.background.Color'blue'";
 
-squares.addEventListener('click', function()){
-    console.log(This)
+function playerMove(){
+unPickedSquareArray.addEventListener('click', playerMove())
+EventTarget = playerPick;
 }
+
+
+
+
