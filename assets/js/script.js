0d7d4cfa6cd7bbc76d1f0f2c95b0c250baf438f5
mejkej <krus3@icloud.com>
2023-02-09 08:21:07 +0000

    working on centering the board then get the reset btn functional and a get winner function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-const board = document.getElementById("board");
-const message = document.getElementById("message");
-const squares = document.getElementByTagName['img'];
+const squares = document.getElementsByClassName("square");
+let playerTurn = true;
 
-let xSquare;
-let compSquare; 
+Array.from(squares).forEach(square => {
+  square.addEventListener("click", function() {
+    if (square.getAttribute("src") === "" && playerTurn) {
+      square.setAttribute("src", "assets/images/x.png");
+      playerTurn = false;
+      computerTurn();
+    }
+  });
+});
 
-
+function computerTurn() {
+  let computerSquare = Math.floor(Math.random() * 9);
+  if (squares[computerSquare].getAttribute("src") === "") {
+    squares[computerSquare].setAttribute("src", "assets/images/o.png");
+    playerTurn = true;
+  } else {
+    computerTurn();
+  }
+}
 
 
 
