16c560ac07f4149147fdd169311c889151b174d4
mejkej <krus3@icloud.com>
2023-02-10 16:25:53 +0000

    cleared the JS will redo it practice makes perfect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,7 @@
-const squares = document.getElementsByClassName("square");
-let playerTurn = true;
+const squares = document.getElementsByClassName('square');
 const resetBtn = document.getElementById('ttt-resetBtn');
 
-resetBtn.addEventListener('click', function(){
-  squares.setAttribute("src", = "")
-})
 
-Array.from(squares).forEach(square => {
-  square.addEventListener("click", function() {
-    if (square.getAttribute("src") === "" && playerTurn) {
-      square.setAttribute("src", "assets/images/x.png");
-      playerTurn = false;
-      computerTurn();
-    }
-  });
-});
-
-function computerTurn() {
-  let computerSquare = Math.floor(Math.random() * 9);
-  if (squares[computerSquare].getAttribute("src") === "") {
-    squares[computerSquare].setAttribute("src", "assets/images/o.png");
-    playerTurn = true;
-  } else {
-    computerTurn();
-  }
-}
-    
-
-function getWinner(){
-
-}
 
 
 
