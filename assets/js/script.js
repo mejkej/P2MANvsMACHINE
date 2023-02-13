7db26d566804325f515a071842c01e7ec412d246
mejkej <krus3@icloud.com>
2023-02-13 04:41:13 +0000

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-const squares = document.getElementsByClassName('square');
 const resetBtn = document.getElementById('ttt-resetBtn');
+const board = document.getElementById('board');
+const message = document.getElementById('message');
+const freeSquares = document.getElementsByClassName('square');
 
+
+Array.from(freeSquares).forEach(square => {
+    square.addEventListener('click', (e) => {
+        if (e.target.src === "assets/images/sqr.png")
+        e.target.src = "assets/images/sqr.png";
+  
+    })
+})
 
 
 
