27f6c2178322217fca5f297bf0673a1db1ee4014
mejkej <krus3@icloud.com>
2023-02-14 14:12:48 +0000

    app.js validated and formatted

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,12 +7,12 @@ const possiblePicks = document.getElementsByClassName('possiblePick');
 
 const resetBtn = document.getElementById('reset-rps');
 
-
-resetBtn.addEventListener('click', function() {
-    playerPoints = 0;
-    compPoints = 0;
-    playerScore.innerHTML = playerPoints;
-    compScore.innerHTML = compPoints;
+// Reset Score 
+resetBtn.addEventListener('click', function () {
+  playerPoints = 0;
+  compPoints = 0;
+  playerScore.innerHTML = playerPoints;
+  compScore.innerHTML = compPoints;
 });
 
 
@@ -22,72 +22,74 @@ let compPoints = 0;
 let playerPick;
 let compPick;
 
+//main loop! 
 Array.from(possiblePicks).forEach(possiblePick => possiblePick.addEventListener('click', (e) => {
-    playerPick = e.target.id;
-    playerHand.src = 'assets/images/' + playerPick + '.png'
-    getCompPick()
-    declareWinner()
-    scoreCount()
-    round()
+  playerPick = e.target.id;
+  playerHand.src = 'assets/images/' + playerPick + '.png';
+  getCompPick();
+  declareWinner();
+  scoreCount();
+  round();
 }));
-
+// Resets the picked hands images and clears the result text after 1.5 seconds
 function round() {
   setTimeout(() => {
     resultText.innerHTML = '';
-      playerHand.src = '';
-        computerHand.src = '';
+    playerHand.src = '';
+    computerHand.src = '';
   }, 1500);
 }
-
+//Generating a randomized computer pick
 function getCompPick() {
-    const randomNumber = Math.floor(Math.random() * 3) + 1
-    if (randomNumber === 1) {
-      compPick = 'rock'
-    }
-    if (randomNumber === 2) {
-      compPick = 'paper'
-    }
-    if (randomNumber === 3) {
-      compPick = 'scissors'
-    }
-    computerHand.src = 'assets/images/' + compPick + '.png';
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    compPick = 'rock';
   }
+  if (randomNumber === 2) {
+    compPick = 'paper';
+  }
+  if (randomNumber === 3) {
+    compPick = 'scissors';
+  }
+  computerHand.src = 'assets/images/' + compPick + '.png';
+}
 
-  function scoreCount(){
-    if (resultText.innerHTML === 'YOU WIN!') {
-    playerPoints++; 
+//function that hands out points to the round winner
+function scoreCount() {
+  if (resultText.innerHTML === 'YOU WIN!') {
+    playerPoints++;
     playerScore.innerHTML = playerPoints;
 
-    }
-    if (resultText.innerHTML === 'YOU LOSE!')
-    compPoints++;
-    compScore.innerHTML = compPoints; 
   }
+  if (resultText.innerHTML === 'YOU LOSE!') {
+    compPoints++;
+    compScore.innerHTML = compPoints;
+  }
+}
 
-  function declareWinner() {
-    if (compPick === playerPick){
-        resultText.innerHTML = 'DRAW!'
-    }
-      if (compPick === 'rock' && playerPick === 'paper'){
-        resultText.innerHTML = 'YOU WIN!'
-      }
-      if (compPick === 'paper' && playerPick === 'scissors'){
-        resultText.innerHTML = 'YOU WIN!'
-      }
-      if (compPick === 'scissors' && playerPick === 'rock'){
-        resultText.innerHTML = 'YOU WIN!'
-      }
-      if (compPick === 'rock' && playerPick === 'scissors'){
-        resultText.innerHTML = 'YOU LOSE!!'
-      }
-      if (compPick === 'paper' && playerPick === 'rock'){
-        resultText.innerHTML = 'YOU LOSE!'
-      }
-      if (compPick === 'scissors' && playerPick === 'paper'){
-        resultText.innerHTML = 'YOU LOSE!'
-      }
-    }
-
-  
+//all possible outcomes of a round and how they should be handled
+function declareWinner() {
+  if (compPick === playerPick) {
+    resultText.innerHTML = 'DRAW!';
+  }
+  if (compPick === 'rock' && playerPick === 'paper') {
+    resultText.innerHTML = 'YOU WIN!';
+  }
+  if (compPick === 'paper' && playerPick === 'scissors') {
+    resultText.innerHTML = 'YOU WIN!';
+  }
+  if (compPick === 'scissors' && playerPick === 'rock') {
+    resultText.innerHTML = 'YOU WIN!';
+  }
+  if (compPick === 'rock' && playerPick === 'scissors') {
+    resultText.innerHTML = 'YOU LOSE!';
+  }
+  if (compPick === 'paper' && playerPick === 'rock') {
+    resultText.innerHTML = 'YOU LOSE!';
+  }
+  if (compPick === 'scissors' && playerPick === 'paper') {
+    resultText.innerHTML = 'YOU LOSE!';
+  }
+}
 
  
