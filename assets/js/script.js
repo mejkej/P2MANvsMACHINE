bba50197efd5ee61a7b83136deb322328d5f6ad7
mejkej <krus3@icloud.com>
2023-02-14 15:40:51 +0000

    added eventlistener for squares clicked square will change squares SRC addedfunctionality to the resetBtn-

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 const resetBtn = document.getElementById('ttt-resetBtn');
-const playerPick = 'x';
-const computerPick = 'o';
+const playerPick = document.querySelectorAll('img[src="assets/images/x.png"]');
+const computerPick = document.querySelectorAll('img[src="assets/images/o.png"]');
 const squares = document.getElementsByClassName('square');
 const message = document.getElementById('message');
 
+Array.from(squares).forEach(square => square.addEventListener('click', (e) => {
+ e.target.src = "assets/images/x.png"
+ e.target = playerPick;
+ getCompPick();
+ 
+}));
 
+getCompPick() {
+const randomNumber = Math.random(squares.length);
+return(randomNumber);
+}
+
+resetBtn.addEventListener('click', function(){
+    location.reload();
+});
