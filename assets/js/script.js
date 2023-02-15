d4cdd4c91d01f11e56fc6b440b4e1a40f01a99d0
mejkej <krus3@icloud.com>
2023-02-15 13:00:12 +0000

    computer random picks after player makes a move

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,22 @@ resetBtn.addEventListener('click', function() {
     location.reload();
 });
 
+
 for (let i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', function () {
     if (squares[i].getAttribute('data-symbol') ==='') { 
     squares[i].src = 'assets/images/x.png';
     squares[i].setAttribute('data-symbol',playerSymbol);
     
+    const unPickedSquare = Array.from(squares).filter(square => square.getAttribute('data-symbol') === '');
+    const randomNumber = Math.floor(Math.random() * unPickedSquare.length);
+    unPickedSquare[randomNumber].src = 'assets/images/o.png';
+    unPickedSquare[randomNumber].setAttribute('data-symbol',computerSymbol);
+
+
+    function winDetector (){
+     
+    }
 
 }})};
 
