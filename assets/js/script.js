0a683ea943a7616745c94e9d872488b73d4aa843
mejkej <krus3@icloud.com>
2023-02-15 16:28:58 +0000

    save

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,36 @@ for (let i = 0; i < squares.length; i++) {
     unPickedSquare[randomNumber].src = 'assets/images/o.png';
     unPickedSquare[randomNumber].setAttribute('data-symbol',computerSymbol);
 
-
-    function winDetector (){
-     
+    const getWinner = winDetector();
+    if(getWinner === playerSymbol){
+        message.innerHTML = 'You Win!'
     }
+    else if (getWinner === computerSymbol){
+        message.innerHTML = 'The Machine Wins!'
+    }
+    }
+})};
 
-}})};
+function winDetector () {
+  const waysToWin = [ 
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+  [0, 4, 8], [2, 4, 6]
+  ];
+
+  for (let i = 0; i < waysToWin.length; i++) {
+    const [a, b, c] = waysToWin[i];
+    if (squares[a].getAttribute('data-symbol') !== '' &&
+        squares[a].getAttribute('data-symbol') === squares[b].getAttribute('data-symbol') &&
+        squares[a].getAttribute('data-symbol') === squares[c].getAttribute('data-symbol')) {
+      if (squares[a].getAttribute('data-symbol') === playerSymbol) {
+        message.innerHTML = 'You Win!';}
+      if (squares[a].getAttribute('data-symbol') === computerSymbol) {
+        message.innerHTML = 'The Machine Wins!';}
+      break;
+    }
+  }
+};
 
 
 
